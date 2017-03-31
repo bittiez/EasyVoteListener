0aa29c4b0a55cd7bc2e85873a37cfdba10024231
Tad Taylor <bittiez@gmail.com>
2017-03-31 15:41:20 -0600

    Adding configurable permissions and commands

--- src/US/bittiez/EasyVoteListener/main.java
+++ src/US/bittiez/EasyVoteListener/main.java
@@ -3,6 +3,9 @@ package US.bittiez.EasyVoteListener;
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -14,39 +17,56 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 import java.util.logging.Logger;
 
-/**
- * Created by tadtaylor on 5/16/16.
- */
-public class main extends JavaPlugin implements Listener{
+public class main extends JavaPlugin implements Listener {
+    private FileConfiguration config = getConfig();
     private FileConfiguration dueConfig;
     private String dueConfigYml = "playersDue.yml";
     private static Logger log = Logger.getLogger("EasyVoteListener");
-    private boolean debug = false;
+    private static boolean debug = true;
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         log = getLogger();
         loadSignData();
-
+        loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
+        if (debug == false)
+            debug = config.getBoolean("debug", false);
     }
 
+    private void loadConfig() {
+        config.options().copyDefaults();
+        saveDefaultConfig();
+    }
 
-    @EventHandler(priority= EventPriority.NORMAL)
+    public boolean onCommand(CommandSender who, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("EVL")) {
+            if (args.length > 0 && args[0].equalsIgnoreCase("test") && who instanceof Player && who.hasPermission("EVL.test")) {
+                who.sendMessage(colorize("&9[EVL] &3Simulating a vote for &b" + who.getName()));
+                rewardPlayer((Player) who);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onVotifierEvent(VotifierEvent event) {
         Vote vote = event.getVote();
         Player who = getServer().getPlayerExact(vote.getUsername());
         log.info(vote.getUsername() + " voted for us!");
-        if(who != null){
-            checkPlayer(who);
-        } else {
-            if(dueConfig.contains(vote.getUsername())){
+
+        if (who != null) { //The player is online
+            rewardPlayer(who);
+        } else { //The player is offline
+            if (dueConfig.contains(vote.getUsername())) { //The player already has a vote pending, add another
                 int t = dueConfig.getInt(vote.getUsername());
                 t++;
                 dueConfig.set(vote.getUsername(), t);
-            } else
+            } else //The player does not have any votes pending, save the first one
                 dueConfig.set(vote.getUsername(), 1);
             saveDueConfig();
         }
@@ -55,50 +75,67 @@ public class main extends JavaPlugin implements Listener{
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
-        if(dueConfig.contains(p.getName())){
+        if (dueConfig.contains(p.getName())) {
             int c = dueConfig.getInt(p.getName());
             for (int i = 0; i < c; i++) {
-                checkPlayer(p);
+                rewardPlayer(p);
             }
             dueConfig.set(p.getName(), null);
             saveDueConfig();
         }
     }
 
-    private void checkPlayer(Player who){
-        if(debug)
+    private void rewardPlayer(Player who) {
+        if (debug)
             log.info("Checking " + who.getName());
-
-        if(who.hasPermission("EVL.inmate")){
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cr givekey " + who.getName() + " inmateKey 1");
-        }
-        if(who.hasPermission("EVL.player")){
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cr givekey " + who.getName() + " playerKey 1");
+        if(config == null)
+            log.severe("Config is null!");
+        Set<String> permSet = config.getConfigurationSection("permissions").getKeys(false);
+        for (String perm : permSet) { //Loop through all permissions
+            if(debug)
+                who.sendMessage(colorize("&9[EVL] &3Testing for permission: "+perm));
+            if (who.hasPermission(perm)) {
+                if (debug)
+                    who.sendMessage(colorize("&9[EVL] &3You have the permission: " + perm));
+                for (String cmd : config.getStringList("permissions." + perm)) { //Loop through all commands for this permission
+                    cmd = cmd.replace("[PLAYER]", who.getName());
+                    if (debug) {
+                        who.sendMessage(colorize("&9[EVL] &3This command would have run if not in debug mode:"));
+                        who.sendMessage(colorize("&9[EVL] &3" + cmd));
+                    } else { //Only run commands when debug mode is disabled
+                        getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
+                    }
+                }
+            }
         }
     }
 
+    private String colorize(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
 
-    private void saveDueConfig(){
+    private void saveDueConfig() {
         try {
             dueConfig.save(new File(this.getDataFolder(), dueConfigYml));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private void loadSignData(){
+
+    private void loadSignData() {
         File dueFile = new File(this.getDataFolder(), dueConfigYml);
 
-        if(!this.getDataFolder().exists())
+        if (!this.getDataFolder().exists())
             this.getDataFolder().mkdirs();
 
-        if(!dueFile.exists()) {
+        if (!dueFile.exists()) {
             try {
                 dueFile.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        if(dueFile.exists())
+        if (dueFile.exists())
             dueConfig = YamlConfiguration.loadConfiguration(dueFile);
         else
             getServer().getPluginManager().disablePlugin(this);
